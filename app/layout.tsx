85015409a39fa625118302298a3686c048606cab
Swapnil Sachan <swapnil.sachan2003@gmail.com>
2024-12-03 20:16:08 +0530

    init: initial commit for openalgorithms client

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 200 300 400 500 600 700 800 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 200 300 400 500 600 700 800 900",
+});
+
+export const metadata: Metadata = {
+  title: "OpenAlgorithms",
+  description: "An open-source platform to learn and practice algorithms and data structures.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
