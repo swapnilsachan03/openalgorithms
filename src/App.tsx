19bc876bb13cc815b374c7d1433779e0c21ea19d
Swapnil Sachan <swapnil.sachan2003@gmail.com>
2025-03-23 17:56:03 +0530

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
+import Navbar from "./components/layout/navbar";
 import Home from "./routes/home/home";
 import Problem from "./routes/problem/problem";
 import NotFound from "./routes/not-found/not-found";
@@ -11,16 +12,19 @@ import Playground from "./routes/playground/playground";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/learn" element={<Learn />} />
-      <Route path="/practice" element={<Practice />} />
-      <Route path="/problem/:slug" element={<Problem />} />
-      <Route path="/playground" element={<Playground />} />
-      <Route path="/interviews" element={<Interviews />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/learn" element={<Learn />} />
+        <Route path="/practice" element={<Practice />} />
+        <Route path="/problem/:slug" element={<Problem />} />
+        <Route path="/playground" element={<Playground />} />
+        <Route path="/interviews" element={<Interviews />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,37 @@
+import _ from "lodash";
+import { Button } from "generic-ds";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+
+const navbarLinks = [
+  { href: "/learn", label: "Learn" },
+  { href: "/practice", label: "Practice" },
+  { href: "/playground", label: "Playground" },
+  { href: "/interviews", label: "Interviews" },
+];
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar__logo">
+        OpenAlgorithms
+      </Link>
+
+      <div className="navbar__container">
+        <div className="navbar__links">
+          {_.map(navbarLinks, link => (
+            <Link key={link.href} to={link.href} className="navbar__link">
+              {link.label}
+            </Link>
+          ))}
+        </div>
+
+        <Button variant="solid" size="medium" label="Sign In" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
