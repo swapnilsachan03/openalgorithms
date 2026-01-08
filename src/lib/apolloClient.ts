03905beb34859a9ebec6b0910917c70b1c63a22d
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2026-01-08 17:19:22 +0530

    chore: upgrade deps

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,9 +1,10 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { HttpLink } from "@apollo/client/link/http";
 
 export const getApolloClient = (token: string | null) => {
   const client = new ApolloClient({
     ssrMode: true,
-    link: createHttpLink({
+    link: new HttpLink({
       uri: `${import.meta.env.VITE_SERVER_URL}/graphql`,
       credentials: "same-origin",
       headers: {
